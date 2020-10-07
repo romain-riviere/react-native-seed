db818cdaf6f007bb6492faafff30cbe9b67785e8
Romain Rivière <romain1.riviere@orange.com>
2020-10-07 23:29:24 +0200

    Added splashscreen

--- app/App.tsx
+++ app/App.tsx
@@ -1,9 +1,20 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import Root from './Root';
 import {AuthContext} from './contexts/AuthContext';
+import RNBootSplash from 'react-native-bootsplash';
 
 function App() {
   let authService = useContext(AuthContext);
+
+  let init = async () => {
+    // …do multiple async tasks
+  };
+
+  useEffect(() => {
+    init().finally(() => {
+      RNBootSplash.hide({duration: 250});
+    });
+  }, []);
 
   return (
     <AuthContext.Provider value={authService}>
